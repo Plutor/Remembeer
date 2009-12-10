255c91edeba204c82f22f7237d3ca1feeceba884
Logan Ingalls <log@plutor.org>
2009-12-09 21:54:32 -0500

    Specify date AND time

--- src/com/wanghaus/beerlog/BeerLog.java
+++ src/com/wanghaus/beerlog/BeerLog.java
@@ -1,6 +1,7 @@
 package com.wanghaus.beerlog;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
@@ -8,17 +9,31 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class BeerLog extends Activity {
-	private static final int DATETIME_DIALOG_ID = 0;
+	private static final int DATE_DIALOG_ID = 0;
+	private static final int TIME_DIALOG_ID = 1;
 	
 	private Spinner drinkWhenSpinner;
+	private int year;
+	private int month;
+	private int date;
 	private int hour;
 	private int minute;
 
+	private DatePickerDialog.OnDateSetListener dateSetListener =
+		new DatePickerDialog.OnDateSetListener() {
+			public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+				year = arg1;
+				month = arg2;
+				date = arg3;
+				showDialog(TIME_DIALOG_ID);
+			}
+	};
 	private TimePickerDialog.OnTimeSetListener timeSetListener =
 	    new TimePickerDialog.OnTimeSetListener() {
 	        public void onTimeSet(TimePicker view, int hourOfDay, int minuteArg) {
@@ -59,7 +74,7 @@ public class BeerLog extends Activity {
 					int selected, long id) {
 				if (selected == 3) {
 					// Show the date time popup
-					showDialog(DATETIME_DIALOG_ID);
+					showDialog(DATE_DIALOG_ID);
 				} else {
 					removeSpecificTime();
 				}
@@ -78,9 +93,12 @@ public class BeerLog extends Activity {
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
-        case DATETIME_DIALOG_ID:
-            return new TimePickerDialog(this,
-                    timeSetListener, hour, minute, false);
+	        case DATE_DIALOG_ID:
+	            return new DatePickerDialog(this,
+	                    dateSetListener, year, month, date);
+	        case TIME_DIALOG_ID:
+	            return new TimePickerDialog(this,
+	                    timeSetListener, hour, minute, false);
         }
         return null;
     }
