c9cf18d80ed32f1c68c258a8dd2a7e1e17e45164
Logan Ingalls <log@plutor.org>
2009-12-11 09:28:04 -0500

    Got the specific date/time option working the way I wanted.

--- src/com/wanghaus/beerlog/BeerLog.java
+++ src/com/wanghaus/beerlog/BeerLog.java
@@ -1,5 +1,9 @@
 package com.wanghaus.beerlog;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
@@ -19,28 +23,31 @@ public class BeerLog extends Activity {
 	private static final int TIME_DIALOG_ID = 1;
 	
 	private Spinner drinkWhenSpinner;
-	private int year;
-	private int month;
-	private int date;
-	private int hour;
-	private int minute;
+	private Calendar specificDate = Calendar.getInstance();
 
 	private DatePickerDialog.OnDateSetListener dateSetListener =
 		new DatePickerDialog.OnDateSetListener() {
 			public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-				year = arg1;
-				month = arg2;
-				date = arg3;
+				specificDate.set(Calendar.YEAR, arg1);
+				specificDate.set(Calendar.MONTH, arg2);
+				specificDate.set(Calendar.DAY_OF_MONTH, arg3);
 				showDialog(TIME_DIALOG_ID);
 			}
 	};
 	private TimePickerDialog.OnTimeSetListener timeSetListener =
 	    new TimePickerDialog.OnTimeSetListener() {
-	        public void onTimeSet(TimePicker view, int hourOfDay, int minuteArg) {
-	            hour = hourOfDay;
-	            minute = minuteArg;
+			@SuppressWarnings("unchecked")
+			public void onTimeSet(TimePicker view, int hourOfDay, int minuteArg) {
+	        	specificDate.set(Calendar.HOUR, hourOfDay);
+	        	specificDate.set(Calendar.MINUTE, minuteArg);
 	            
-	            // TODO - Add to the drinkWhenSpinner dropdown
+	            // Add to the drinkWhenSpinner dropdown and select it
+	        	ArrayAdapter<CharSequence> drinkWhenAdapter = (ArrayAdapter<CharSequence>) drinkWhenSpinner.getAdapter();
+	        	while (drinkWhenAdapter.getCount() > 4)
+	        		drinkWhenAdapter.remove( drinkWhenAdapter.getItem(4) );
+	        	
+	        	drinkWhenAdapter.add( DateFormat.getDateTimeInstance().format(specificDate.getTime()) );
+	        	drinkWhenSpinner.setSelection( drinkWhenAdapter.getCount() - 1 );
 	        }
 	    };
 	
@@ -52,30 +59,39 @@ public class BeerLog extends Activity {
         
         // Beer name autocomplete text field
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.beername);
-        ArrayAdapter beerNameAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> beerNameAdapter = ArrayAdapter.createFromResource(this,
                 R.array.beernames, android.R.layout.simple_dropdown_item_1line);
         textView.setAdapter(beerNameAdapter);
      
         // Containers dropdown
         Spinner containerSpinner = (Spinner) findViewById(R.id.container);
-        ArrayAdapter containerAdapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> containerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.containers, android.R.layout.simple_spinner_item);
         containerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         containerSpinner.setAdapter(containerAdapter);
         
+        // Initialize the specificDate
+        specificDate.setTime( new Date() );
+        
         // Drinkwhen dropdown
         drinkWhenSpinner = (Spinner) findViewById(R.id.drinkwhen);
-        ArrayAdapter drinkWhenAdapter = ArrayAdapter.createFromResource(this,
-                R.array.drinkwhens, android.R.layout.simple_spinner_item);
+    	ArrayAdapter<CharSequence> drinkWhenAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
         drinkWhenAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         drinkWhenSpinner.setAdapter(drinkWhenAdapter);
+        
+        // Add all the things from R.array.drinkwhens
+        String[] drinkWhens = getResources().getStringArray(R.array.drinkwhens);
+        for (String s : drinkWhens) {
+        	drinkWhenAdapter.add(s);
+        }
+        
         drinkWhenSpinner.setOnItemSelectedListener( new OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int selected, long id) {
 				if (selected == 3) {
 					// Show the date time popup
 					showDialog(DATE_DIALOG_ID);
-				} else {
+				} else if (selected < 3) {
 					removeSpecificTime();
 				}
 			}
@@ -84,8 +100,12 @@ public class BeerLog extends Activity {
 				removeSpecificTime(); 
 			}
 			
+			@SuppressWarnings("unchecked")
 			public void removeSpecificTime() {
-				// TODO - Remove the 4th option if it exists
+				// Remove the 4th option if it exists
+	        	ArrayAdapter<CharSequence> drinkWhenAdapter = (ArrayAdapter<CharSequence>) drinkWhenSpinner.getAdapter();
+	        	while (drinkWhenAdapter.getCount() > 4)
+	        		drinkWhenAdapter.remove( drinkWhenAdapter.getItem(4) );
 			}
         });
     }
@@ -95,10 +115,18 @@ public class BeerLog extends Activity {
         switch (id) {
 	        case DATE_DIALOG_ID:
 	            return new DatePickerDialog(this,
-	                    dateSetListener, year, month, date);
+                    dateSetListener,
+                    specificDate.get(Calendar.YEAR),
+                    specificDate.get(Calendar.MONTH),
+                    specificDate.get(Calendar.DAY_OF_MONTH)
+                );
 	        case TIME_DIALOG_ID:
 	            return new TimePickerDialog(this,
-	                    timeSetListener, hour, minute, false);
+                    timeSetListener,
+                    specificDate.get(Calendar.HOUR),
+                    specificDate.get(Calendar.MINUTE),
+                    false
+                );
         }
         return null;
     }
