43ddb7749f08a64eb8e0bdb6f204b5eb9d395a0c
Logan Ingalls <log@plutor.org>
2009-12-21 11:05:36 -0500

    Make deleting history work

--- src/com/wanghaus/beerlog/activity/History.java
+++ src/com/wanghaus/beerlog/activity/History.java
@@ -1,7 +1,13 @@
 package com.wanghaus.beerlog.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,18 +22,21 @@ import com.wanghaus.beerlog.R;
 public class History extends BaseActivity {
 	static final int SHOW_DETAILS = 1;
 	static final int DELETE = 2;
+	static final int DO_DELETE = 3;
+	
+	ListView historyList;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
         
+        historyList = (ListView)findViewById(R.id.history_list);
+        
         // Get the last ten beers
         Cursor lastTenBeers = db.query(DB_TABLE,
         		new String[] {"ROWID AS _id", "beername", "container || ' at ' || stamp AS details"},
         		null, null, null, null, "stamp DESC", "10");
-        
-        ListView list = (ListView)findViewById(R.id.history_list);
         
         // Map Cursor columns to views defined in simple_list_item_2.xml
         ListAdapter historyAdapter = new SimpleCursorAdapter(this,
@@ -35,10 +44,10 @@ public class History extends BaseActivity {
                         new String[] { "beername", "details" }, 
                         new int[] { android.R.id.text1, android.R.id.text2 });
 
-        list.setAdapter(historyAdapter);
+        historyList.setAdapter(historyAdapter);
         
         /* Add Context menu listener to the ListView. */
-        list.setOnCreateContextMenuListener(new OnCreateContextMenuListener() {
+        historyList.setOnCreateContextMenuListener(new OnCreateContextMenuListener() {
              public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {  
                   menu.setHeaderTitle("ContextMenu");
                   menu.add(0, SHOW_DETAILS, 0, "Details");
@@ -50,6 +59,7 @@ public class History extends BaseActivity {
     @Override
     public boolean onContextItemSelected(MenuItem aItem) {
          AdapterContextMenuInfo menuInfo = (AdapterContextMenuInfo) aItem.getMenuInfo();
+         final long beerId = menuInfo.id;
 
          /* Switch on the ID of the item, to get what the user selected. */
          switch (aItem.getItemId()) {
@@ -58,8 +68,20 @@ public class History extends BaseActivity {
              
              return true; /* true means: "we handled the event". */
          case DELETE:
-             // TODO
-             
+    	     new AlertDialog.Builder(this)
+    	       .setTitle("Delete this beer?")
+    	       .setMessage("This action cannot be undone.")
+    	       .setPositiveButton((CharSequence)"Delete", new OnClickListener() {
+					public void onClick(DialogInterface arg0, int arg1) {
+						db.execSQL("DELETE FROM " + DB_TABLE + " WHERE ROWID = " + String.valueOf(beerId));
+						// Update the view
+						SimpleCursorAdapter listAdapter = (SimpleCursorAdapter) historyList.getAdapter();
+						listAdapter.getCursor().requery();
+					}
+    	        })  
+    	        .setNegativeButton("Cancel", null)
+    	        .show();
+
              return true; /* true means: "we handled the event". */
          }
          return false;
