fc8c5c9e96975a410500950866dd279f257c7491
Logan Ingalls <log@plutor.org>
2009-12-21 14:25:40 -0500

    Add OptionsMenu with a button for settings

--- src/com/wanghaus/beerlog/activity/BaseActivity.java
+++ src/com/wanghaus/beerlog/activity/BaseActivity.java
@@ -1,13 +1,18 @@
 package com.wanghaus.beerlog.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class BaseActivity extends Activity {
 	protected static final String DB_NAME = "BeerLog";
 	protected static final String DB_TABLE = "beer_log";
 	protected SQLiteDatabase db = null;
+	
+	private final int MENU_SETTINGS = 1;
 	
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,4 +37,26 @@ public class BaseActivity extends Activity {
     	}
     }
 
+	/* Creates the menu items */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!super.onCreateOptionsMenu(menu))
+			return false;
+		
+	    menu.add(0, MENU_SETTINGS, 0, "Settings")
+	    	.setIcon(android.R.drawable.ic_menu_preferences);
+
+	    return true;
+	}
+
+	/* Handles item selections */
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case MENU_SETTINGS:
+	    	Intent nextIntent = new Intent(this, Config.class);
+	    	startActivity(nextIntent);
+
+	        return true;
+	    }
+	    return false;
+	}
 }
