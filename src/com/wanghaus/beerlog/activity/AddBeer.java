c631b4663e08e8fc04254e65adbd74264545f9ae
Matt Anderson <mra@malloc.org>
2010-01-04 14:18:07 -0500

    Revert "Zero out seconds when specifying a time"
    This reverts commit 1c9aa55c333df6b638104dcea01ed19b4e305173.

--- src/com/wanghaus/beerlog/activity/AddBeer.java
+++ src/com/wanghaus/beerlog/activity/AddBeer.java
@@ -211,7 +211,6 @@ public class AddBeer extends BaseActivity {
 			public void onTimeSet(TimePicker view, int hourOfDay, int minuteArg) {
 	        	specificTime.set(Calendar.HOUR, hourOfDay);
 	        	specificTime.set(Calendar.MINUTE, minuteArg);
-				specificTime.set(Calendar.SECOND, 0);
 	            
 	            // Add to the drinkWhenSpinner dropdown and select it
 	        	ArrayAdapter<CharSequence> drinkWhenAdapter = (ArrayAdapter<CharSequence>) drinkWhenSpinner.getAdapter();
