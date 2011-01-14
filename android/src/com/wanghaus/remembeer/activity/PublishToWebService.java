c479f2c6b6ce08c1b496c0069d9cbf82ac68e684
Matt Anderson <mra@malloc.org>
2011-01-14 06:13:21 -0500

    Add some text to the publish screen, we still need a throbber

--- android/src/com/wanghaus/remembeer/activity/PublishToWebService.java
+++ android/src/com/wanghaus/remembeer/activity/PublishToWebService.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wanghaus.remembeer.R;
@@ -20,6 +21,7 @@ public class PublishToWebService extends BaseActivity {
 		final BeerDbHelper dbs;
 		final WebServiceHelper wsh;
 		final Context cContext;
+		final Integer Count;
 		
 		setTitle(R.string.webService_warning_title);
 		setContentView(R.layout.publish_to_webservice);
@@ -27,6 +29,25 @@ public class PublishToWebService extends BaseActivity {
 		cContext = this;
 		dbs = new BeerDbHelper(this);
 		wsh = new WebServiceHelper(this);
+		Count = dbs.getDrinkCountUnPublished();
+
+		TextView webServiceUploadText = (TextView) findViewById(R.id.webService_upload_text);
+		String webServiceUploadStr = getText(R.string.webService_upload_prefix).toString();
+		webServiceUploadStr += Count.toString();
+		webServiceUploadStr += getText(R.string.webService_upload_suffix).toString();
+		webServiceUploadText.setText(webServiceUploadStr);
+		
+		TextView webServiceEstimateText = (TextView) findViewById(R.id.webService_estimate_text);
+		String webServiceEstimateStr = getText(R.string.webService_publish_prefix).toString();
+		webServiceEstimateStr += " ";
+		if (Count > 99) {
+			webServiceEstimateStr += getText(R.string.webService_publish_5m).toString();
+		} else if (Count > 60) {
+			webServiceEstimateStr += getText(R.string.webService_publish_3m).toString();
+		} else
+			webServiceEstimateStr += getText(R.string.webService_publish_1m).toString();
+		
+		webServiceEstimateText.setText(webServiceEstimateStr);
 		
         // Publish!
         Button yesButton = (Button) findViewById(R.id.webService_warning_yes);
@@ -34,18 +55,16 @@ public class PublishToWebService extends BaseActivity {
             public void onClick(View view) {
             	// Yes
         		
-            	// Use dbs to get a count of unpublished drinks
-            	// get a query of unpublished drinks
-            	// send them via sendWebServiceRequest(Drink drink)
-            	// Do it properly with a throbber in a new context view
-            	Integer count = dbs.getDrinkCountUnPublished();
-            	Toast.makeText(cContext, "Publishing " + count.toString() + " Beers", Toast.LENGTH_LONG).show();
-            	
+            	// We should really do this
+            	// properly with a throbber in a new context view
+
             	List<Drink> ListOfDrinks = dbs.getDrinks("uuid is null", null, null);
-            	for (int i = 0; i < count; i++) {
+            	for (int i = 0; i < Count; i++) {
             		wsh.sendWebServiceRequest(ListOfDrinks.get(i));
             	}
             	
+            	Toast.makeText(cContext, "Published " + Count.toString() + " Beers", Toast.LENGTH_LONG).show();
+
         		setResult(1);
             	finish();
             }
